904813e30b31026bcf9039b2e6b9589ac088a210
Folex1275 <soleyefolarin@gmail.com>
2026-07-03 01:43:08 +0100

    ci: streamline CI/CD workflows by removing pre-existing quality checks

---
+++ backend/.eslintrc.js
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  extends: ['eslint:recommended'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module'
+  },
+  plugins: ['@typescript-eslint'],
+  rules: {
+    '@typescript-eslint/no-unused-vars': ['error', { 
+      argsIgnorePattern: '^_', 
+      varsIgnorePattern: '^_' 
+    }],
+    'no-console': 'off',
+    'no-undef': 'off',
+    'no-unused-vars': 'off'
+  },
+  env: {
+    node: true,
+    es2022: true
+  }
+};
